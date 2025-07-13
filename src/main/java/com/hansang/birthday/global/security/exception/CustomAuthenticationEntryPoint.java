2d82a0e43f26fab4a00eaf81ea3d475b954e3445
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-07-13 18:37:58 +0900

    feat: 예외처리 설정

--- src/main/java/com/hansang/birthday/global/security/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/hansang/birthday/global/security/exception/CustomAuthenticationEntryPoint.java
@@ -5,13 +5,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Component
-public class CustomAuthenticationEntryPoint {
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.setContentType("application/json;charset=UTF-8");
