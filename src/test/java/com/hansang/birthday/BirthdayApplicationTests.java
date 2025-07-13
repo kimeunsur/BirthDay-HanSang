b4a9de736cb861c78f0201b18f41c5cdab0d32e2
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-07-13 16:36:09 +0900

    Initial commit: Spring Boot project

---
+++ src/test/java/com/hansang/birthday/BirthdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansang.birthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
